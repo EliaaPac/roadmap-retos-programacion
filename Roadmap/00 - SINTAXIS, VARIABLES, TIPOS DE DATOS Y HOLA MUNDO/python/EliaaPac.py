06d3765cda0e697b2bb00c36304bb7bdf74ed891
EliaaPac <eliasspalma20@gmail.com>
2024-11-02 17:36:33 -0600

    Create EliaaPac.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EliaaPac.py
@@ -0,0 +1,63 @@
+# https://www.python.org/
+
+"""
+Esto tambien es un comentario en varias lineas 
+
+"""
+
+'''
+
+Esto tambien es un comentario en varias lineas 
+
+'''
+
+my_variable = "Variable cadena de texto"
+my_variable = "Esto variable tambien es un cadena de texto"
+
+#se guarda e imprime el ultimo dato que se le da a la variable
+
+print (my_variable)
+
+MI_CONSTANTE = "Mi constante" #por convencion (indica que no deberia ser modificada, pyton no lo tiene pero java y c++ si)
+
+#Tipos de datos primiticos -int,float,boolean,string
+
+#Numeros Enteros (int)
+my_int= 7
+
+#numero decimal (float)
+
+my_float= 7.1
+
+#Booleano (devuelve verdadero o falso)
+
+my_bool= False
+
+#Cadena de texto (string)
+
+my_string= "Cadena de texto"
+
+print("Hola, he programado en python ")
+
+
+#Ejercicio fuera del curso
+
+nombre_string = "Elias"
+Edad_int = 22
+Altura_float= 1.72
+es_estuduiante_booleano= True
+
+print("Cual es tu nombre  " +  nombre_string, 
+      "cual es tu edad  "  + str  (Edad_int), 
+      "Cual es tu altura  "  + str  (Altura_float), 
+      "Eres estudiante?  "  + str   (es_estuduiante_booleano))
+
+
+nombre_string = "Yo"
+Edad_int = 22
+Altura_float= 1.72
+es_estuduiante_booleano= False
+
+print("Cual es tu nombre  " +  nombre_string, "\n" "cual es tu edad  "  + str  (Edad_int), "\n" "Cual es tu altura  "  + str  (Altura_float),"\n" "Eres estudiante?  "  + str   (es_estuduiante_booleano))
+
+
